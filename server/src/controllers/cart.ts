47566693999342a4411e84372d0a20a037da317d
Yakubu Lute <yakubulute@outlook.com>
2024-12-23 17:41:45 -1200

    Added a cron jobs for carts

--- server/src/controllers/cart.ts
+++ server/src/controllers/cart.ts
@@ -208,23 +208,138 @@ export const addToCart = async (req: Request, res: Response) => {
     )
     if (!updatedProduct) {
       await session.abortTransaction()
-      return res.status(400).json({ message: 'Product out of stock or concurrency issue' })
+      return res
+        .status(400)
+        .json({ message: 'Product out of stock or concurrency issue' })
     }
     await session.commitTransaction()
-    res.status(200).json({ message: 'Product added to cart successfully' , data:newCartItem})
+    res.status(200).json({
+      message: 'Product added to cart successfully',
+      data: newCartItem
+    })
     session.endSession()
   } catch (error) {
     console.log('Error adding to cart', error)
     await session.abortTransaction()
     res.json(500).json({ message: 'Internal server error', error })
-  }                                    
+  } finally {
+    await session.endSession()
+  }
 }
 
 export const modifyProductQuantity = async (req: Request, res: Response) => {
   try {
-  } catch (error) {}
+    const { id, cartProductId } = req.params
+    const { quantity } = req.body
+    if (!id || !cartProductId) {
+      return res
+        .status(400)
+        .json({ message: 'User ID and Cart Product ID are required' })
+    }
+    const user = await User.findById(id)
+    if (!user) {
+      return res.status(404).json({ message: 'User not found' })
+    }
+    let cartProduct = await CartProduct.findById(cartProductId)
+
+    if (!cartProduct) {
+      return res.status(404).json({ message: 'Product not found' })
+    }
+
+    const product = await Product.findById(cartProduct.product)
+    if (!product) {
+      return res.status(404).json({ message: 'Product not found' })
+    }
+    if (product.countInStock < quantity) {
+      return res.status(400).json({ message: 'Product out of stock' })
+    }
+    cartProduct = await CartProduct.findByIdAndUpdate(cartProductId, quantity, {
+      new: true
+    })
+    if (!cartProduct) {
+      return res
+        .status(400)
+        .json({ message: 'Error modifying product quantity' })
+    }
+    res.json({
+      message: 'Product quantity modified successfully',
+      data: cartProduct
+    })
+  } catch (error) {
+    console.log('Error modifying product quantity:', error)
+    res.status(500).json({ message: 'Internal server error', error })
+  }
 }
+
 export const removeFromCart = async (req: Request, res: Response) => {
+  const session = await mongoose.startSession()
+  session.startTransaction()
+
   try {
-  } catch (error) {}
+    const { id, cartProductId } = req.params
+    if (!id || !cartProductId) {
+      return res
+        .status(400)
+        .json({ message: 'User ID and Cart Product ID are required' })
+    }
+    const user = await User.findById(id)
+    if (!user) {
+      await session.abortTransaction()
+      return res.status(404).json({ message: 'User not found' })
+    }
+    if (!user.cart.includes(cartProductId as any)) {
+      await session.abortTransaction()
+      return res.status(404).json({ message: 'Product not in the users cart' })
+    }
+
+    const cartProductToRemove = await CartProduct.findById(cartProductId)
+    if (!cartProductToRemove) {
+      await session.abortTransaction()
+      return res.status(404).json({ message: 'Product not found' })
+    }
+
+    if (cartProductToRemove.reserved) {
+      await Product.findOneAndUpdate(
+        { _id: cartProductToRemove.product },
+        { $inc: { countInStock: cartProductToRemove.quantity } },
+        { new: true }
+      )
+        .session(session)
+        .then(() => {
+          console.log('Product stock updated')
+        })
+        .catch(async error => {
+          console.log('Error updating product stock')
+          await session.abortTransaction()
+          res.status(500).json({ message: 'Internal server error', error })
+        })
+    }
+      
+    // Find the index of the product to remove
+    const productIndex = user.cart.findIndex(product => product.id === cartProductToRemove.id);
+
+    // If the product is found, remove it from the cart
+    if (productIndex !== -1) {
+      user.cart.splice(productIndex, 1);
+    }
+
+    await user.save({ session });
+    const cartProduct = await CartProduct.findByIdAndDelete(cartProductToRemove.id).session(session);
+    if (!cartProduct) {
+      await session.abortTransaction();
+      return res.status(404).json({ message: 'Product not found' });
+    }
+    
+    await session.commitTransaction()
+    res.json({
+      message: 'Product removed from cart successfully',
+      data: cartProduct
+    })
+
+  } catch (error) {
+    console.log('Error removing product from cart:', error)
+    res.status(500).json({ message: 'Internal server error', error })
+  } finally {
+    session.endSession()
+  }
 }
--- server/src/utils/cron-jobs.ts
+++ server/src/utils/cron-jobs.ts
@@ -1,6 +1,8 @@
 import cron from 'node-cron'
 import Category from '../models/category'
 import { Product } from '../models/product'
+import { CartProduct } from '../models/cart-products'
+import mongoose from 'mongoose'
 
 // cron job to delete categories marked for deletion at 12:00 am every day
 export const cronJobs = () => {
@@ -25,6 +27,56 @@ export const cronJobs = () => {
       console.log('Categories marked for deletion deleted successfully')
     } catch (error: any) {
       console.error('Crone job error:', error)
+    }
+  })
+
+  // cron job to sweep the server for only the carts collection for every 30 mins
+  cron.schedule('*/30 * * * *', async () => {
+    const session = await mongoose.startSession()
+    session.startTransaction()
+    try {
+      console.log(`Cart reservation release CRON job started at `, new Date())
+      const expiredCartReservation = await CartProduct.find({
+        reserved: true,
+        reservationExpiry: { $lt: new Date() }
+      }).session(session)
+      for (const cartProduct of expiredCartReservation) {
+        const product = await Product.findById(cartProduct.product).session(
+          session
+        )
+        if (product) {
+          const updatedProduct = await Product.findByIdAndUpdate(
+            product._id,
+            { $inc: { countInStock: product.quantity } },
+            {
+              new: true,
+              runValidators: true,
+              session
+            }
+          )
+          if (!updatedProduct) {
+            console.log('Product update failed')
+            await session.abortTransaction()
+            return
+          }
+        }
+        await CartProduct.findByIdAndUpdate(
+          cartProduct._id,
+          {
+            reserved: false
+          },
+          { session }
+        )
+      }
+      await session.commitTransaction()
+      console.log(
+        'Cart reservation release CRON job completed successfully',
+        new Date()
+      )
+    } catch (error: any) {
+      console.error('Crone job error:', error)
+    } finally {
+      await session.endSession()
     }
   })
 }
